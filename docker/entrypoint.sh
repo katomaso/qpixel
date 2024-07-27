b959bf2e55586ca944640be2b49d9be57c5d99c3
Tomas Peterka <tomas@peterka.me>
2024-07-27 19:29:57 +0200

    Apply sos-cesko patch

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+set -ex
 
-if [ ! -f "/tmp/db-created" ]; then
+if [ ! -f "/code/config/db-created" ]; then
     echo "Creating database"
     rails db:create
-    rails db:schema:load
+    # rails db:schema:load
     echo "Migrating database"
     rails db:migrate
     rails r db/scripts/create_tags_path_view.rb
@@ -11,12 +12,20 @@ if [ ! -f "/tmp/db-created" ]; then
     rails r docker/create_admin_and_community.rb
     echo "Seeding database"
     UPDATE_POSTS=true rails db:seed
-    touch /tmp/db-created
+    touch /code/config/db-created
+else
+    echo "Migrating database"
+    rails db:migrate
+
+    # I think we should not seed multiple times
+    # If this isn't done again, there is a 500 error on the first page about posts
+    # echo "Seeding database"
+    # rails db:seed
 fi
 
-# If this isn't done again, there is a 500 error on the first page about posts
-echo "Seeding database"
-rails db:seed
+if [ ! -f "public/assets/*.css" ]; then
+    bundle exec rails assets:precompile
+fi
 
 # defaults to port 3000
 echo "Starting server 0.0.0.0:3000"
