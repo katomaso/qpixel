06a801332f1044055d59c0e1a7373b3404c37309
Tomas Peterka <tomas@peterka.me>
2024-11-17 20:24:35 +0100

    fix: More custom-functional modifications

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
-set -x
+set -ex
 
-# If not created yet
-if [ ! -f "/code/tmp/db-created" ]; then
+if [ ! -f "/code/config/db-created" ]; then
+    echo "Creating database"
     rails db:create
-    rails db:schema:load
+    # rails db:schema:load
+    echo "Migrating database"
     rails db:migrate
     rails r db/scripts/create_tags_path_view.rb
     rails r docker/create_admin_and_community.rb
     echo "Seeding database"
     UPDATE_POSTS=true rails db:seed
-    touch /code/tmp/db-created
+    touch /code/config/db-created
+else
+    echo "Migrating database"
+    rails db:migrate
+
+    # I think we should not seed multiple times
+    # If this isn't done again, there is a 500 error on the first page about posts
+    # echo "Seeding database"
+    # rails db:seed
 fi
 
-# If this isn't done again, there is a 500 error on the first page about posts
-echo "Seeding database"
-rails db:seed
+if [ ! -f "public/assets/*.css" ]; then
+    bundle exec rails assets:precompile
+fi
 
 # defaults to port 3000
 echo "Starting server 0.0.0.0:3000"
